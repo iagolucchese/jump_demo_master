c5ce8ad6358e2ed787fd827a502a58a86cadf392
Bueno Lucchese, Iago <iagobuen@indiana.edu>
2014-09-09 16:17:23 -0400

    new scene, messing with camera/mouse programming

---
+++ Assets/Scripts/CameraFollow2.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow2 : MonoBehaviour {
+
+	public Camera cam;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!cam)
+			return;
+
+		if (cam.transform.position.y < transform.position.y) {
+			cam.transform.position = new Vector3(cam.transform.position.x, transform.position.y, cam.transform.position.z);
+		}
+	}
+}
